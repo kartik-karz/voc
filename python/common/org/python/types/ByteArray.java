7bea5a7d41528df6e2f180ef7084d1f4904db7bc
Kartik Kulkarni <kartik.koolks@gmail.com>
2018-03-05 17:19:00 +0530

    modified isdigit

--- python/common/org/python/types/ByteArray.java
+++ python/common/org/python/types/ByteArray.java
@@ -776,7 +776,27 @@ public class ByteArray extends org.python.types.Object {
             __doc__ = "B.isdigit() -> bool\n\nReturn True if all characters in B are digits\nand there is at least one character in B, False otherwise."
     )
     public org.python.Object isdigit(java.util.List<org.python.Object> args, java.util.Map<java.lang.String, org.python.Object> kwargs, java.util.List<org.python.Object> default_args, java.util.Map<java.lang.String, org.python.Object> default_kwargs) {
-        throw new org.python.exceptions.NotImplementedError("bytearray.isdigit has not been implemented.");
+        // throw new org.python.exceptions.NotImplementedError("bytearray.isdigit has not been implemented.");
+        if(this.value.length==0){
+          return new org.python.types.Bool(0);
+        }
+        char charByte;
+        boolean charFlag = false;
+        for(byte bt: this.value){
+          charByte = (char) bt;
+          if(Character.isDigit(charByte)){
+            continue;
+          }
+          else{
+            charFlag = true;
+          }
+        }
+        if(charFlag){
+          return new org.python.types.Bool(0);
+        }
+        else{
+          return new org.python.types.Bool(1);
+        }
     }
 
     @org.python.Method(
